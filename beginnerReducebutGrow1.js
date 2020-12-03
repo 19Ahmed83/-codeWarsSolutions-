d5a978d126b9e11a78b5872e775142bb3705da27
@hmed <spb6926@gmail.com>
2020-12-03 19:16:05 +0100

    solution

---
+++ beginnerReducebutGrow1.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+function grow(x){
+    let res=1;
+    for(let i = 0; i < x.length; i++){
+        res *=x[i];
+    }
+    return res;
+
+}
