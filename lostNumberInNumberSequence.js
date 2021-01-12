4568e4a1c36c3e52bb6e276e5290232e257c1e7c
@hmed <spb6926@gmail.com>
2021-01-12 21:20:36 +0100

    solution

--- lostNumberInNumberSequence.js
+++ lostNumberInNumberSequence.js
@@ -1,5 +1,6 @@
 //https://www.codewars.com/kata/595aa94353e43a8746000120/train/javascript
-function findDleteNumber(arr,mixArr){
-    return arr.reduce((acc,curr)=>acc+curr,0)-mixArr((acc,curr)=>acc+curr,0);
+function findDleteNumber(arr, mixArr) {
+    return arr.reduce((acc, curr) => acc + curr, 0) - mixArr((acc, curr) => acc + curr, 0);
 }
+
 //console.log([1,2,3,4,5], [3,4,1,5])===>2 Deletion
---
+++ takeAnArrowToTheKneeFunctionally.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
+const ArrowFunc = function(arr) {
+    return arr.map(el =>String.fromCharCode(el) ).join('');
+}
