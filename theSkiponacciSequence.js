1c5a499b57228a03fc5c2bb44c23da932d72ff77
@hmed <spb6926@gmail.com>
2021-01-16 18:38:24 +0100

    solution

---
+++ theSkiponacciSequence.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/580777ee2e14accd9f000165/train/javascript
+function skiponacci(n) {
+    if (n === 1) return '1'
+    let arr = [1,1]
+    for (let i = 0; i < n - 2; i++) {
+        arr.push(arr[i] + arr[i+1])
+    }
+    return arr.map((v,i) => (i+1) % 2 === 0 ? 'skip' : v).join` `
+}
